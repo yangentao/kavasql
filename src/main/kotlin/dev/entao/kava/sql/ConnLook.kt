9c41646693550ad4dac429020906a1740b59de2a
yangentao <entaoyang@163.com>
2019-04-19 18:47:12 +0800

    1.0.1

--- src/main/kotlin/dev/entao/kava/sql/ConnLook.kt
+++ src/main/kotlin/dev/entao/kava/sql/ConnLook.kt
@@ -1,5 +1,6 @@
 package dev.entao.kava.sql
 
+import dev.entao.kava.base.Task
 import java.sql.Connection
 import javax.naming.InitialContext
 import javax.sql.DataSource
@@ -7,53 +8,59 @@ import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotation
 
 object ConnLook {
-	private const val prefix = "java:comp/env/jdbc"
-	private val ctx = InitialContext()
-	private val nameList = ArrayList<String>()
+    private const val prefix = "java:comp/env/jdbc"
+    private val ctx = InitialContext()
+    private val nameList = ArrayList<String>()
 
-	private val threadLocal: ThreadLocal<HashMap<String, Connection>> = object : ThreadLocal<HashMap<String, Connection>>() {
-		override fun initialValue(): HashMap<String, Connection> {
-			return HashMap()
-		}
-	}
+    private val threadLocal: ThreadLocal<HashMap<String, Connection>> =
+        object : ThreadLocal<HashMap<String, Connection>>() {
+            override fun initialValue(): HashMap<String, Connection> {
+                return HashMap()
+            }
+        }
 
-	init {
-		val ls = ctx.list(prefix)
-		while (ls.hasMore()) {
-			val a = ls.next()
-			nameList += a.name
-		}
-	}
+    init {
+        val ls = ctx.list(prefix)
+        while (ls.hasMore()) {
+            val a = ls.next()
+            nameList += a.name
+        }
+        Task.setCleanBlock("connlook", this::cleanThreadConnections)
+    }
 
-	fun removeThreadLocal() {
-		val map = threadLocal.get()
-		for (c in map.values) {
-			c.close()
-		}
-		map.clear()
-		threadLocal.remove()
-	}
+    fun cleanThreadConnections() {
+        removeThreadLocal()
+    }
 
-	fun named(name: String): Connection {
-		val map = threadLocal.get()
-		val c = map[name]
-		if (c != null) {
-			return c
-		}
+    fun removeThreadLocal() {
+        val map = threadLocal.get()
+        for (c in map.values) {
+            c.close()
+        }
+        map.clear()
+        threadLocal.remove()
+    }
 
-		val defaultDataSource = ctx.lookup("$prefix/$name") as DataSource
-		val cc = defaultDataSource.connection
-		map[name] = cc
-		return cc
-	}
+    fun named(name: String): Connection {
+        val map = threadLocal.get()
+        val c = map[name]
+        if (c != null) {
+            return c
+        }
 
-	val first: Connection get() = named(nameList.first())
+        val defaultDataSource = ctx.lookup("$prefix/$name") as DataSource
+        val cc = defaultDataSource.connection
+        map[name] = cc
+        return cc
+    }
 
-	fun named(modelClass: KClass<*>): Connection {
-		val cname = modelClass.findAnnotation<dev.entao.kava.sql.ConnName>() ?: return first
-		if (cname.value.isEmpty()) {
-			return first
-		}
-		return named(cname.value)
-	}
+    val first: Connection get() = named(nameList.first())
+
+    fun named(modelClass: KClass<*>): Connection {
+        val cname = modelClass.findAnnotation<dev.entao.kava.sql.ConnName>() ?: return first
+        if (cname.value.isEmpty()) {
+            return first
+        }
+        return named(cname.value)
+    }
 }
